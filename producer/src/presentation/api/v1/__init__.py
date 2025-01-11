32d7b206b412114af336e674349a8895b3422a9b
vfaddey <vfaddey@yandex.ru>
2025-01-11 19:50:54 +0300

    api endpoints

---
+++ producer/src/presentation/api/v1/__init__.py
@@ -0,0 +1,3 @@
+from src.presentation.api.v1.files import router as files_router
+from src.presentation.api.v1.users import router as users_router
+from src.presentation.api.v1.cards import router as cards_router
