2629702c8a27f5066db0851efab8d092b9385bcb
vfaddey <vfaddey@yandex.ru>
2025-01-18 18:48:07 +0300

    схемы pydantic

--- producer/src/presentation/schemas/card.py
+++ producer/src/presentation/schemas/card.py
@@ -4,21 +4,23 @@ from typing import Optional
 from pydantic import BaseModel, UUID4
 
 from src.domain.entities import CardStatus
+from src.domain.entities.card import CardType
 
 
 class CreateCardSchema(BaseModel):
     file_id: UUID4
-    card_type: str
+    card_type: CardType
 
 
 class CardSchema(BaseModel):
     id: UUID4
     file_id: UUID4
-    card_type: str
+    card_type: CardType
+    card_type_translation: Optional[str]
     markdown_text: str
     status: CardStatus
     user_id: UUID4
-    result: Optional[dict] = None
+    result: Optional[dict] = {}
 
     created_at: Optional[datetime]
     updated_at: Optional[datetime]
