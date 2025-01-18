b17a1bc6d8e7f16ac9c0736fcba632ee53038e98
vfaddey <vfaddey@yandex.ru>
2025-01-18 18:47:43 +0300

    добавил метод перевода из sqla модели в сущность

--- producer/src/infrastructure/repositories/card_repository.py
+++ producer/src/infrastructure/repositories/card_repository.py
@@ -21,15 +21,7 @@ class SqlaCardRepository(CardRepository):
             self._session.add(card_db)
             await self._session.commit()
             await self._session.refresh(card_db)
-            return Card(id=card_db.id,
-                        card_type=card_db.card_type,
-                        user_id=card_db.user_id,
-                        status=card_db.status,
-                        markdown_text=card_db.markdown_text,
-                        file_id=card_db.file_id,
-                        created_at=card_db.created_at,
-                        updated_at=card_db.updated_at,
-                        result=card_db.result)
+            return self.__to_entity(card_db)
         except SQLAlchemyError as e:
             await self._session.rollback()
             raise e
@@ -40,29 +32,13 @@ class SqlaCardRepository(CardRepository):
         card_db = result.scalars().first()
         if not card_db:
             raise CardNotFound(f'No such card with id {card_id}')
-        return Card(id=card_db.id,
-                    card_type=card_db.card_type,
-                    user_id=card_db.user_id,
-                    status=card_db.status,
-                    markdown_text=card_db.markdown_text,
-                    file_id=card_db.file_id,
-                    created_at=card_db.created_at,
-                    updated_at=card_db.updated_at,
-                    result=card_db.result)
+        return self.__to_entity(card_db)
 
     async def get_by_user_id(self, user_id: UUID) -> list[Card]:
         stmt = select(CardModel).where(CardModel.user_id == user_id)
         result = await self._session.execute(stmt)
         cards_db = result.unique().scalars().all()
-        return [Card(id=card_db.id,
-                     card_type=card_db.card_type,
-                     user_id=card_db.user_id,
-                     status=card_db.status,
-                     markdown_text=card_db.markdown_text,
-                     file_id=card_db.file_id,
-                     created_at=card_db.created_at,
-                     updated_at=card_db.updated_at,
-                     result=card_db.result) for card_db in cards_db]
+        return [self.__to_entity(card_db) for card_db in cards_db]
 
 
     async def update(self, card: Card) -> Card:
@@ -77,17 +53,7 @@ class SqlaCardRepository(CardRepository):
             await self._session.commit()
             await self._session.refresh(card_db)
 
-            return Card(
-                id=card_db.id,
-                card_type=card_db.card_type,
-                user_id=card_db.user_id,
-                status=card_db.status,
-                markdown_text=card_db.markdown_text,
-                file_id=card_db.file_id,
-                created_at=card_db.created_at,
-                updated_at=card_db.updated_at,
-                result=card_db.result,
-            )
+            return self.__to_entity(card_db)
         except SQLAlchemyError as e:
             await self._session.rollback()
             raise e
@@ -100,8 +66,12 @@ class SqlaCardRepository(CardRepository):
         card_db = result.scalars().first()
         if not card_db:
             raise CardNotFound(f'No such card with id {card_id}')
+        return self.__to_entity(card_db)
+
+    def __to_entity(self, card_db: CardModel) -> Card:
         return Card(id=card_db.id,
                     card_type=card_db.card_type,
+                    card_type_translation=card_db.card_type_translation,
                     user_id=card_db.user_id,
                     status=card_db.status,
                     markdown_text=card_db.markdown_text,
