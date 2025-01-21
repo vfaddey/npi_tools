2f243dda202805519567b736c800ad58a0b539c3
vfaddey <vfaddey@yandex.ru>
2025-01-21 21:35:44 +0300

    group repository

---
+++ producer/src/domain/repositories/group_repository.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+from uuid import UUID
+
+from src.domain.entities.group import Group
+
+
+class GroupRepository(ABC):
+
+    @abstractmethod
+    async def create(self, group: Group) -> Group:
+        raise NotImplementedError
+
+
+    @abstractmethod
+    async def get(self, group_id: UUID) -> Group:
+        raise NotImplementedError
+
+
+    @abstractmethod
+    async def get_by_user_id(self, user_id: UUID) -> list[Group]:
+        raise NotImplementedError
+
+
+    @abstractmethod
+    async def update_name(self, group_id: UUID, group: Group) -> Group:
+        raise NotImplementedError
+
+
+    @abstractmethod
+    async def delete(self, group_id: UUID) -> Group:
+        raise NotImplementedError
