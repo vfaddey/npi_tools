2fb16bbc26131c33258ec66bd639bd2c1dc1463e
vfaddey <vfaddey@yandex.ru>
2025-01-22 00:32:59 +0300

    group endpoints

--- producer/src/presentation/api/v1/__init__.py
+++ producer/src/presentation/api/v1/__init__.py
@@ -1,3 +1,4 @@
 from src.presentation.api.v1.files import router as files_router
 from src.presentation.api.v1.users import router as users_router
 from src.presentation.api.v1.cards import router as cards_router
+from src.presentation.api.v1.groups import router as groups_router
