4ad587a1c2d34f2f9c08d8d57e77ab0d4b1c1d9f
vfaddey <vfaddey@yandex.ru>
2025-01-22 00:31:53 +0300

    group service

---
+++ producer/src/application/services/group_service.py
@@ -0,0 +1,38 @@
+from uuid import UUID
+
+from src.application.exceptions.groups import NotAGroupOwner
+from src.domain.entities import Group
+from src.domain.repositories.group_repository import GroupRepository
+
+
+class GroupService:
+    def __init__(self, repository: GroupRepository):
+        self.__repository = repository
+
+    async def create(self, group: Group) -> Group:
+        return await self.__repository.create(group)
+
+    async def get_by_id(self, group_id: UUID) -> Group:
+        return await self.__repository.get(group_id)
+
+    async def get_by_user_id(self, user_id: UUID) -> list[Group]:
+        return await self.__repository.get_by_user_id(user_id)
+
+    # async def update(self, group: Group) -> Group:
+    #     return await self.__repository.update(group.id, group)
+
+    async def rename_group(self, group_id: UUID, new_name: str) -> Group:
+        group_ex = await self.__repository.get(group_id)
+        group_ex.name = new_name
+        updated = await self.__repository.update_name(group_id, group_ex)
+        return updated
+
+    async def delete(self, group_id: UUID, user_id: UUID) -> Group:
+        group_ex = await self.__repository.get(group_id)
+        if group_ex.user_id != user_id:
+            raise NotAGroupOwner('You are not allowed to access this group')
+        return await self.__repository.delete(group_id)
+
+    async def add_card_to_group(self, group_id: UUID, card_id: UUID) -> Group:
+        ...
+
