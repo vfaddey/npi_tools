d99d9160260104576d5b93dd9d80f32d7375f9d9
vfaddey <vfaddey@yandex.ru>
2025-01-30 14:25:10 +0300

    шэринг и копии карточек

--- producer/src/domain/repositories/card_repository.py
+++ producer/src/domain/repositories/card_repository.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 from uuid import UUID
 
-from src.domain.entities.card import Card
+from src.domain.entities.card import Card, SharingURL, CardCopy
 
 
 class CardRepository(ABC):
@@ -19,9 +19,31 @@ class CardRepository(ABC):
         raise NotImplementedError
 
     @abstractmethod
+    async def get_by_sharing_code(self, code: str) -> Card:
+        raise NotImplementedError
+
+    @abstractmethod
     async def update(self, card: Card) -> Card:
         raise NotImplementedError
 
     @abstractmethod
     async def delete(self, card_id: UUID):
+        raise NotImplementedError
+
+
+    @abstractmethod
+    async def create_sharing_url(self, sharing_url: SharingURL) -> SharingURL:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def get_sharing_url_by_card_id(self, card_id: UUID) -> SharingURL:
+        raise NotImplementedError
+
+
+    @abstractmethod
+    async def get_card_copy(self, card_id: UUID, user_id: UUID) -> Card:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def create_card_copy(self, card_copy: CardCopy) -> CardCopy:
         raise NotImplementedError
