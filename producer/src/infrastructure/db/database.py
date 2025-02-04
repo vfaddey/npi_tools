cd48d0f676c311dc7d02ccfffbd1636e191d84f8
vfaddey <vfaddey@yandex.ru>
2025-02-05 02:37:14 +0300

    обновил базовый класс бд

--- producer/src/infrastructure/db/database.py
+++ producer/src/infrastructure/db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sess
 from sqlalchemy.orm import declarative_base
 from src.infrastructure.config import settings
 
-engine = create_async_engine(settings.DB_URL, echo=True)
+engine = create_async_engine(settings.DB_URL, echo=False)
 AsyncSessionFactory = async_sessionmaker(
     bind=engine, class_=AsyncSession, expire_on_commit=False
 )
