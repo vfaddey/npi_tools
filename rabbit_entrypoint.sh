1b1b8fb4d0f8f73c11d35ea6aa76dfaefc789d07
vfaddey <vfaddey@yandex.ru>
2025-02-19 16:06:22 +0300

    entrypoint rabbit

--- rabbit_entrypoint.sh
+++ rabbit_entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -e
 
-
-echo "Запуск RabbitMQ..."
+echo "Запуск RabbitMQ (detached)..."
 rabbitmq-server -detached
 
 echo "Ожидание запуска RabbitMQ..."
@@ -14,19 +13,16 @@ RABBITMQ_DEFAULT_USER=${RABBITMQ_DEFAULT_USER:-admin}
 RABBITMQ_DEFAULT_PASS=${RABBITMQ_DEFAULT_PASS:-password}
 VHOST="my_vhost"
 
-
 if ! rabbitmqctl list_users | grep -q "^${RABBITMQ_DEFAULT_USER}"; then
   echo "Создание пользователя ${RABBITMQ_DEFAULT_USER}..."
   rabbitmqctl add_user "${RABBITMQ_DEFAULT_USER}" "${RABBITMQ_DEFAULT_PASS}"
   rabbitmqctl set_user_tags "${RABBITMQ_DEFAULT_USER}" administrator
 fi
 
-
 if ! rabbitmqctl list_vhosts | grep -q "^${VHOST}"; then
   echo "Создание виртуального хоста ${VHOST}..."
   rabbitmqctl add_vhost "${VHOST}"
 fi
-
 
 echo "Установка разрешений для пользователя ${RABBITMQ_DEFAULT_USER}..."
 rabbitmqctl set_permissions -p "${VHOST}" "${RABBITMQ_DEFAULT_USER}" ".*" ".*" ".*"
@@ -36,5 +32,5 @@ echo "Перезапуск приложения RabbitMQ..."
 rabbitmqctl stop_app
 rabbitmqctl start_app
 
-echo "Запуск RabbitMQ в переднем плане..."
-rabbitmq-server
+echo "RabbitMQ запущен. Следим за логами..."
+tail -f /var/log/rabbitmq/*log
