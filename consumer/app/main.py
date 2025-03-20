5232a59297d3b92173d303f7c9fb185b75eba73d
vfaddey <vfaddey@yandex.ru>
2025-03-20 19:40:58 +0300

    cardtype

--- consumer/app/main.py
+++ consumer/app/main.py
@@ -1,5 +1,4 @@
 # {"id": "72e82323-3863-43cd-95d8-bf3a04f8937b", "file_id": "cfb684be-14da-499a-8fab-71d3b0d4f189", "card_type": "pvt"}
-import asyncio
 
 from fastapi import FastAPI
 
@@ -7,6 +6,7 @@ from app.adapters.minio_client import client as minio_client
 from app.adapters.rabbitmq_client import RabbitMQConsumer
 from app.card_handlers.base.handler_manager import HandlerManager
 from app.card_handlers.pseudosoil.pseudosoil_card import PseudosoilHandler
+from app.card_handlers.simple_gdis_calculate.simple_gdis_card import SimpleGDISHandler
 from app.core.config import settings
 from app.db.database import AsyncSessionFactory
 from app.services.card_service import CardServiceFactory
@@ -14,8 +14,8 @@ from app.services.card_service import CardServiceFactory
 app = FastAPI()
 
 
-handler_manager = HandlerManager(PseudosoilHandler
-                                 )
+handler_manager = HandlerManager(PseudosoilHandler,
+                                 SimpleGDISHandler)
 card_service_factory = CardServiceFactory(AsyncSessionFactory,
                                           minio_client,
                                           handler_manager)
